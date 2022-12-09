c8b8b1f26ddb97ef1b56054764af99d52257e8bb
Pedram Hajesmaeeli <pedram.hajesmaeeli@glovoapp.com>
2022-12-09 16:46:02 +0100

    Update base url

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:memolang/main_with_api_url.dart';
 
 void main() {
-  mainWithApiUrl(
-      apiUrl:
-          "http://memolang-env.eba-y2m8mcve.us-east-1.elasticbeanstalk.com");
+  mainWithApiUrl(apiUrl: "https://api.memolang.app");
 }
